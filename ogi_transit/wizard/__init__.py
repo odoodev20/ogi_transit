ee949999d9e5940641c3f0cbba8c2953343db6cd
odoodev20 <odoodeveloper2.0@gmail.com>
2026-08-16 15:34:12 +0600

    Delete ogi_transit directory

--- ogi_transit/wizard/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import reason_wizard
-from . import payment_wizard
-from . import pl_import_wizard
-from . import vendor_payment_wizard
-from . import assign_forwarder_wizard
-from . import reminder_wizard
